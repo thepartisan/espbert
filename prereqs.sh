3b2e7a9953067d010b73d7e7ee51c1400f6878ec
Christian Schirling <schirchris@gmx.de>
2018-12-31 13:08:12 +0100

    added prereqs script

---
+++ prereqs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export WORKSPACE=$(pwd)
+git submodule add https://github.com/espressif/esp-idf.git esp-idf
+cd esp-idf
+git submodule update --init
+cd ${WORKSPACE}
+export IDF_PATH=${WORKSPACE}/esp-idf
+curl -o xtensa-esp32-elf-linux64.tar.gz https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+bsdtar xf xtensa-esp32-elf-linux64.tar.gz
+
+export PATH=/path/to/xtensa-esp32-elf/bin:$PATH
+
+git submodule add https://github.com/MrBuddyCasino/ESP32_MP3_Decoder.git ESP32_MP3_Decoder
+cd ESP32_MP3_Decoder/
+git submodule update --init
+cd ${WORKSPACE}
